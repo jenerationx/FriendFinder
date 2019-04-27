b0ebf0586758a96cb8c98b2a30c7c168274991d2
jenerationx <jenerationx74@gmail.com>
2019-04-26 20:00:18 -0400

    fix image

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
-
+app.use(express.static("app/public"));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
